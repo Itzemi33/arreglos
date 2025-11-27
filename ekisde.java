43834a875f3d5ffbfedfbe6d59410116fdcc9f8e
itzemi33 <fnietoc2400@alumno.ipn.mx>
2025-11-27 13:20:47 -0600

    Maldita

--- ekisde.java
+++ ekisde.java
@@ -1,6 +1,6 @@
 public class ekisde {
     public static void main(String[] args) {
-        int [][] arr = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        int [][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         System.out.println("Hola");
         for(int i = 0;i < arr.length;i++)
         {
@@ -21,15 +21,29 @@ public class ekisde {
         }
 
 
-        for(int j = 0;j < arr.length;j++)
+        for(int j = 0;j < arr[0].length;j++)
         {
             int sumcol = 0;
-            for (int i = 0; i < arr[j].length; i++)
+            for (int i = 0; i < arr.length; i++)
             {
                 sumcol += arr [i][j];
             }
             System.out.println("Columna: " + (j+1) + " ; Suma: " + sumcol);
         }
 
+
+        int resultado = 0;
+        for(int x = 0;x < arr.length;x++)
+        {
+            resultado += arr [x][x];
+        }
+        System.out.println("Suma diagonal 1: " + resultado);
+
+        int resultado2 = 0;
+        for(int x = 0;x < arr.length;x++)
+        {
+            resultado2 += arr [(arr.length - 1) - x][x];
+        }
+        System.out.println("Suma diagonal 2: " + resultado2);
     }
 }
